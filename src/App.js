c29704da3c0e666f766a89bbd6850e4a82fd76f4
David Maximiliano Moyano <davidmaximilianomoyano@gmail.com>
2022-06-17 21:46:50 -0300

    Se realiza seccion del clima

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
 import img1 from './Assets/Img/Varias-pruebas/1.jpg'
-import CardGrande from './Component/Cards/CardGrande/CardGrande';
-import CardMediana from './Component/Cards/CardMediana/CardMediana';
-import CardPequeña from './Component/Cards/CardPequeña/CardPequeña';
-
 import React from 'react';
+import Weather from './Component/Weather/Weather';
+import CardGiant from './Component/Cards/CardGiant/CardGrande';
+import CardMedium from './Component/Cards/CardMedium/CardMedium';
+import CardLittle from './Component/Cards/CardLittle/CardLittle';
 
 const App = () => {
   return (
     <div>
+        <Weather />
+         <CardGiant img= {img1} seccion= 'No lo entenderias' text_p= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500, ' />
 
-         <CardGrande img= {img1} titulo= 'No lo entenderias' texto_parrafo= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500, ' />
+        <div className="seccion-news">
+         <CardLittle img= {img1} title= 'Espectaculos' text_p= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,' />
 
-         <CardPequeña img= {img1} titulo= 'Espectaculos' texto_parrafo= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,' />
+         <CardLittle img= {img1} title= 'Espectaculos' text_p= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,' />
 
-         <CardMediana  img= {img1} seccion= 'Comedias' titulo='lo que nunca vimos' texto_parrafo= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,' />
+         <CardLittle img= {img1} title= 'Espectaculos' text_p= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,' />
+        </div>
+        
+        <div className="seccion-sports">
+         <CardMedium  img= {img1} seccion= 'Comedias' title='lo que nunca vimos' text_p= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,' />
+
+         <CardLittle img= {img1} title= 'Espectaculos' text_p= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,' />
+
+         <CardLittle img= {img1} title= 'Espectaculos' text_p= 'Lorem Ipsum es simplemente un texto ficticio de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto ficticio estándar de la industria desde el año 1500,' />
+        </div>
     </div>
   );
 };
