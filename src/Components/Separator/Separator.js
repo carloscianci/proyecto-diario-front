af5e43b8f08cc5e7b9690bf4888ae821f5981641
unknown <davidmaximilianomoyano@gmail.com>
2022-06-23 16:29:30 -0300

    Se crean separadores de noticias por seccion

--- src/Components/Separator/Separator.js
+++ src/Components/Separator/Separator.js
@@ -3,9 +3,15 @@ import './Separator.css'
 
 const Separator = (props) => {
     return (
-        <div className='separator_style'>
-            <h3 className='text-separator'>{props.seccion}</h3>
-        </div>
+        <>
+            <div className='separator_style'>
+                <h3 className='text-separator'>{props.seccion}</h3>
+            </div>
+
+            <div className='container'>
+                <hr className='hr_style' />
+            </div>
+        </>
     );
 };
 
