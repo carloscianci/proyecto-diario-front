d0ff0beca0df447c0a9aa4f364e1d68ae2e7d1e2
Carlos Cianci <carloscianci@gmail.com>
2022-06-29 21:59:02 -0300

    Agrego y personalizo navbar

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ import React from 'react';
 import Weather from './Components/Weather/Weather';
 import Footer from './Components/Footer/Footer';
 import GetApiNoticias from './Components/GetApiNoticias/GetApiNoticias';
+import Navbar from './Components/Navbar/Navbar';
 
 
 
 const App = () => {
 
   return (
+    <>
+      <Navbar />
       <div className='container-fluid'>
-        <div >
-        
-          <div className='col-12'>
-          <Weather />
+          <div >
+          
+            <div className='col-12'>
+            <Weather />
+            </div>
+
+
+            <GetApiNoticias />
+          
+
+          
+            <Footer />
           </div>
 
-
-          <GetApiNoticias />
-        
-
-        
-          <Footer />
-        </div>
-
-    </div>
+      </div>
+    </>
   );
 }
 
