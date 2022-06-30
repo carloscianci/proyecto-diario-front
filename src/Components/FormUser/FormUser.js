5d8cbdeddc0bfbbd49cf5fc0124fd708e82abc29
Mariano <garzonma98@gmail.com>
2022-06-30 20:52:38 -0300

    correcciones para la conexion con back y funciones

--- src/Components/FormUser/FormUser.js
+++ src/Components/FormUser/FormUser.js
@@ -13,30 +13,24 @@ const FormUser = () => {
       "nombre" : dataForm.nombre,
       "apellido" : dataForm.apellido,
       "email" : dataForm.email,
-      "celular" : dataForm.celular
+      "telefono" : dataForm.telefono
     })
-    if (resultsFU && resultsFU.dataForm.resultado) {
-      localStorage.setItem("nombre", resultsFU.dataForm.nombre)
+    // e.target.reset()
+    if(resultsFU && resultsFU.data.resultado){
+      alert('Se ha registrado con exito.')
+    }else{
+      alert(resultsFU.data.message)
     }
-    if (resultsFU && resultsFU.dataForm.resultado) {
-      localStorage.setItem("apellido", resultsFU.dataForm.apellido)
-    }
-    if (resultsFU && resultsFU.dataForm.resultado) {
-      localStorage.setItem("email", resultsFU.dataForm.email)
-    }
-    if (resultsFU && resultsFU.dataForm.resultado) {
-      localStorage.setItem("celular", resultsFU.dataForm.celular)
-    }
-    
-    e.target.reset()
   }
+
+  
 
   return (
     <>
       <div className='d-flex flex-column justify-content-center align-items-center titulo titulo-1'><h2>¿Desea saber más sobre lo que pasa en el mundo?</h2></div>
       <div className='d-flex flex-column justify-content-center align-items-center titulo titulo-2'><h2>Suscríbase y permanezca informado junto a nosotros!</h2></div>
       <div className='d-flex flex-column justify-content-center align-items-center container-formulario'>
-        <form className='d-flex flex-column justify-content-center align-items-center form-container'>
+        <form onSubmit={handleSubmit(postFormUser)} className='d-flex flex-column justify-content-center align-items-center form-container'>
           <div className='d-flex justify-content-center'>
             <div className='item-registro'>
               <label for='nombreCompleto' className='form-label'>Nombre/s: </label>
@@ -64,13 +58,13 @@ const FormUser = () => {
           </div>
           <div className='item-registro'>
             <label for='numeroTel' className='form-label'>Celular: </label>
-            <input type='text' className='form-control' id='numeroTel' placeholder='N° de telefono' required {...register("celular", {
+            <input type='text' className='form-control' id='numeroTel' placeholder='N° de telefono' required {...register("telefono", {
                         pattern:/[0-9]+/g
                       })}
             />
           </div>
           <div>
-            <button onClick={handleSubmit(postFormUser)} className='btn btn-primary boton-registro'>Suscribirse</button>
+            <button type="submit" className='btn btn-primary boton-registro'>Suscribirse</button>
           </div>
         </form>
       </div>
