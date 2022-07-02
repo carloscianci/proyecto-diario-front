f406e64e02b6c97c39340eab11090f6175b55673
Mariano <garzonma98@gmail.com>
2022-07-02 18:34:39 -0300

    separar los items de nombre y apellido

--- src/Components/FormUser/FormUser.js
+++ src/Components/FormUser/FormUser.js
@@ -24,8 +24,6 @@ const FormUser = () => {
     }
   }
 
-  
-
   return (
     <>
       <div className="espacio-titulos">
@@ -35,33 +33,33 @@ const FormUser = () => {
       <div className='d-flex flex-column justify-content-center align-items-center container-formulario'>
         <form onSubmit={handleSubmit(postFormUser)} className='d-flex flex-column justify-content-center align-items-center form-container'>
           <div className='d-flex justify-content-center justify-content-between item-registro'>
-            <div>
+            <div className='d-flex flex-column'>
               <label for='nombreCompleto' className='form-label'>Nombre/s: </label>
-              <input type='text' className='form-control' id='nombreCompleto' placeholder='Nombre completo' required 
+              <input type='text' className='form-control items-pequeños' id='nombreCompleto' placeholder='Nombre completo' required 
                 {...register("nombre",{
                     pattern:/[a-zA-Z]+/gm
                 })}
               />
             </div>
-            <div>
+            <div className='d-flex flex-column'>
               <label for='apellidoCompleto' className='form-label'>Apellido/s: </label>
-              <input type='text' className='form-control' id='apellidoCompleto' placeholder='Apellido completo' required 
+              <input type='text' className='form-control items-pequeños' id='apellidoCompleto' placeholder='Apellido completo' required 
                 {...register("apellido",{
                   pattern:/[a-zA-Z]+/gm
                 })}
               />
             </div>
           </div>
-          <div className='item-registro padding-items'>
+          <div className='item-registro padding-items flex-column'>
             <label for='email' className='form-label'>E-mail: </label>
             <input type='email' className='form-control' id='email' placeholder='correoelectronico@example.com' required {...register("email", {
                         pattern:/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
                       })}
             />
           </div>
-          <div className='item-registro padding-items'>
+          <div className='d-flex justify-content-start item-registro flex-column'>
             <label for='numeroTel' className='form-label'>Celular: </label>
-            <input type='text' className='form-control' id='numeroTel' placeholder='N° de telefono' required {...register("telefono", {
+            <input type='text' className='form-control items-pequeños' id='numeroTel' placeholder='N° de telefono' required {...register("telefono", {
                         pattern:/[0-9]+/g
                       })}
             />
