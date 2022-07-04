e3a0063dc3812044e82228c957a78591c441be47
Mariano <garzonma98@gmail.com>
2022-07-03 22:24:20 -0300

    css en gral del page y se incluyo un boton

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './error404.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Error404 = () =>{
   return(
     <>
-      <section className='container-error404 d-flex justify-content-center align-items-center'>
-        <div className=''>
-          <h1>ERROR404!!</h1>
-          <h2>
-            A causa de un error en nuestros servidores no podremos brindarle nueva información.
-            Intente de nuevo más tarde.
-          </h2>
+      <section className='container-error404 d-flex flex-column align-items-center'>
+        <div className='div-texto-error d-flex flex-column align-items-center'>
+          <h1 className='margin-titulo-pc fuente-titulo-error-pc fuente-titulo-error-mobile d-flex flex-column align-items-center margin-titulo-mobile'>ERROR </h1>
+          <h1 className='fuente-titulo-error-pc fuente-titulo-error-mobile d-flex flex-column align-items-center'>404</h1>
         </div>
+        
+        <div className='fuente-texto-error-mobile d-flex flex-column align-items-center'>
+          <h3 className='texto-mobile'>
+            No se ha podido encontrar la página que está buscando.
+          </h3>
+          <h3 className='texto-mobile'>
+            Por favor, intente de nuevo más tarde.
+          </h3>
+        </div>
+
+        <div className="boton-retornar-principal">
+          <a type="submit" className='btn btn-primary' href="/#">Volver a página principal</a>
+        </div>
+
       </section>
     </>
   );
